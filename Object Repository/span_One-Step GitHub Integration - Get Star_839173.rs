91d4b0a69e9346aae13e9ab9d67ad78e161b25d5
Maciej <Maciej@Maciej-PC>
2021-10-16 13:27:35 +0200

    zad1

---
+++ Object Repository/span_One-Step GitHub Integration - Get Star_839173.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_One-Step GitHub Integration - Get Star_839173</name>
+   <tag></tag>
+   <elementGuidId>e34b6d10-78c2-4ee7-a409-3361059e9325</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.cfxYMc.JfZTW.c4Djg.MUxGbd.v0nnCb > span</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='tads']/div/div/div/div/a/div/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>One-Step GitHub Integration - Get Started for Free - CircleCI.com</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;tads&quot;)/div[@class=&quot;uEierd&quot;]/div[1]/div[@class=&quot;cUezCb luh4tb O9g5cc uUPGi&quot;]/div[@class=&quot;d5oMvf&quot;]/a[@class=&quot;Krnil&quot;]/div[@class=&quot;cfxYMc JfZTW c4Djg MUxGbd v0nnCb&quot;]/span[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='tads']/div/div/div/div/a/div/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Reklamy'])[1]/following::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='·'])[1]/preceding::span[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='One-Step GitHub Integration - Get Started for Free - CircleCI.com']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a/div/span</value>
+   </webElementXpaths>
+</WebElementEntity>
